f163a97abc46f9814dff3885b275f12f5c5bc438
Andrey Ryshkevich <135042756+andreirysh@users.noreply.github.com>
2023-06-09 17:53:17 +0200

    styled

--- src/App.js
+++ src/App.js
@@ -2,40 +2,43 @@ import React from "react";
 
 function App() {
   return (
-    <div className="wrapper">
-      <header>
-        <div className="headerLeft">
-          <img src="/img/logo.png"/>
-          <div className="headerInfo">
-            <h3>React Sneakers</h3>
+    <div className="wrapper clear">
+      <header className="d-flex justify-between align-center p-40">
+        <div className="d-flex align-center">
+          <img src="/img/logo.png" />
+          <div >
+            <h3 className="text-uppercase">React Sneakers</h3>
             <p>Магазин лучших кроссовок</p>
           </div>
         </div>
-        <ul className="headerRight">
-          <li>
-          <img src="/img/cart.svg"/>
+        <ul className="d-flex">
+          <li className="mr-30">
+            <img src="/img/cart.svg" />
             <span>1205 руб.</span>
           </li>
           <li>
-          <img src="/img/user.svg"/>
+            <img src="/img/user.svg" />
           </li>
         </ul>
       </header>
-      <div className="content">
+      <div className="content p-40">
         <h1>Все кроссовки</h1>
-        <div className="card">
-        <img src="/img/sneakers/1.jpg"/>
-        <p>Мужские Кроссовки Nike Blazer Mid Suede</p>
-        <div>
-          <div>
-            <span>Цена:</span>
-            <b>12 999 руб.</b>
+        <div className="d-flex">
+          <div className="card">
+            <img width={130} height={110} src="/img/sneakers/1.jpg" />
+            <h5>Мужские Кроссовки Nike Blazer Mid Suede</h5>
+            <div className="d-flex justify-between align-center">
+              <div className="d-flex flex-column">
+                <span>Цена:</span>
+                <b>12 999 руб.</b>
+              </div>
+              <button className="button">
+                <img src="/img/plus.svg"></img>
+              </button>
+            </div>
           </div>
-          <button>
-            <img src="/img/plus.svg"></img>
-          </button>
         </div>
-        </div>
+
       </div>
     </div>
   );
